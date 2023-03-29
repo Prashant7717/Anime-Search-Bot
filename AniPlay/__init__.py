e78cb6a878f25ddc7d641e90c088ad0f092130e1
Prashant7717 <105821189+Prashant7717@users.noreply.github.com>
2023-03-29 10:06:16 -0400

    Update __init__.py

--- AniPlay/__init__.py
+++ AniPlay/__init__.py
@@ -3,7 +3,7 @@ from pyrogram.client import Client
 
 app = Client(
     "AniPlay",
-    api_id= int(environ.get("APP_ID", "")),
-    api_hash= environ.get("API_HASH", ""),
-    bot_token= environ.get("TOKEN", "")
+    api_id= int(environ.get("APP_ID", "29728392")),
+    api_hash= environ.get("API_HASH", "2b3ee4e6b67b568b09b1b6560dba30af"),
+    bot_token= environ.get("TOKEN", "5924561801:AAF29EvpFXj6-BNmtL7cTiE9Ud5uDcrwdek")
 )
